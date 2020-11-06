792e1bba338b04468a6909e7b26742f14ded2e8e
mearnic <1335666107@qq.com>
2020-11-06 15:25:04 +0800

    解决framelayout的问题

--- LoanMarket/app/src/main/java/com/market/loan/adapter/MainLoanRecyclerViewAdapter.java
+++ LoanMarket/app/src/main/java/com/market/loan/adapter/MainLoanRecyclerViewAdapter.java
@@ -1,16 +1,20 @@
 package com.market.loan.adapter;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.market.loan.R;
+import com.market.loan.bean.Duration;
 import com.market.loan.bean.Limit;
 import com.market.loan.tools.AmountFormat;
 
@@ -33,10 +37,23 @@ public class MainLoanRecyclerViewAdapter extends RecyclerView.Adapter<MainLoanRe
         return new ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Limit limit = values.get(position);
+        final Limit limit = values.get(position);
         holder.amountText.setText(AmountFormat.format(limit.getAmount()));
+        List<Duration> durations = limit.getDurations();
+        holder.daysText.setText(durations.get(0).getDuration().split(" ")[0] + "-" + durations.get(durations.size() - 1).getDuration());
+
+        View.OnClickListener onClickListener = new View.OnClickListener(
+        ) {
+            @Override
+            public void onClick(View v) {
+                // todo
+                Log.d(ContentValues.TAG, "销毁了:" + limit.toString());
+            }
+        };
+        holder.layout.setOnClickListener(onClickListener);
     }
 
     @Override
@@ -46,10 +63,15 @@ public class MainLoanRecyclerViewAdapter extends RecyclerView.Adapter<MainLoanRe
 
     static class ViewHolder extends RecyclerView.ViewHolder {
         AppCompatTextView amountText;
+        AppCompatTextView daysText;
+        ConstraintLayout layout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             amountText = itemView.findViewById(R.id.loanAmount);
+            daysText = itemView.findViewById(R.id.loanDays);
+            daysText = itemView.findViewById(R.id.loanDays);
+            layout = itemView.findViewById(R.id.loanListItem);
         }
     }
 }
