92893787da18dcb2dfb014adb3108f1c9277e8ee
mearnic <1335666107@qq.com>
2020-11-09 12:18:38 +0800

    优化代码

--- LoanMarket/app/src/main/java/com/market/loan/BaseActivity.java
+++ LoanMarket/app/src/main/java/com/market/loan/BaseActivity.java
@@ -1,39 +1,21 @@
 package com.market.loan;
 
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.graphics.drawable.ColorDrawable;
-import android.view.KeyEvent;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Objects;
-
 public class BaseActivity extends AppCompatActivity {
 
-  private AlertDialog alertDialog;
+    private AlertDialog loadDialog;
 
-  public void show() {
-      alertDialog = new AlertDialog.Builder(this).create();
-      Objects.requireNonNull(alertDialog.getWindow()).setBackgroundDrawable(new ColorDrawable());
-      alertDialog.setCancelable(false);
-      alertDialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
-          @Override
-          public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-              if (keyCode == KeyEvent.KEYCODE_SEARCH || keyCode == KeyEvent.KEYCODE_BACK)
-                  return true;
-              return false;
-          }
-      });
-      alertDialog.show();
-      alertDialog.setContentView(R.layout.loading);
-      alertDialog.setCanceledOnTouchOutside(false);
-  }
+    public void show() {
+        loadDialog = new AlertDialog.Builder(this, R.style.TransparentDialog).create();
+        loadDialog.setCancelable(false);
+        loadDialog.show();
+        loadDialog.setContentView(R.layout.loading);
+        loadDialog.setCanceledOnTouchOutside(false);
+    }
 
-  public void dismiss() {
-      if (null != alertDialog && alertDialog.isShowing()) {
-          alertDialog.dismiss();
-      }
-  }
+    public void dismiss() {
+        loadDialog.dismiss();
+    }
 }
